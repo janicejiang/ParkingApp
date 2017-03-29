5919b40a162354c9af230f415f0f90e3da117278
janice <janicejiang.d@gmail.com>
2017-03-29 17:37:07 +0800

    part2

---
+++ spec/models/parking_spec.rb
@@ -0,0 +1,67 @@
+require 'rails_helper'
+
+RSpec.describe Parking, type: :model do
+  # pending "add some examples to (or delete) #{__FILE__}"
+  describe ".validate_end_at_with_amount" do
+    it "is invalid without amount" do
+      parking = Parking.new(:parking_type => "guest",
+                            :start_at => Time.now - 6.hours,
+                            :end_at => Time.now)
+      expect(parking).to_not be_valid
+    end
+
+    it "is invalid without end_at" do
+      parking = Parking.new(:parking_type => "guest",
+                            :start_at => Time.new - 6.hours,
+                            :amount => 999)
+      expect(parking).to_not be_valid
+    end
+  end
+
+  describe ".calculate_amount" do
+    it "30 mins should be 2 RMB" do
+      t = Time.now
+      parking = Parking.new(:parking_type => "guest",
+                            :start_at => t,
+                            :end_at => t + 30.minutes)
+      parking.calculate_amount
+      expect(parking.amount).to eq(200)
+    end
+
+    it "60 mins should be 2 RMB" do
+      t = Time.now
+      parking = Parking.new(:parking_type => "guest",
+                            :start_at => t,
+                            :end_at => t + 60.minutes)
+      parking.calculate_amount
+      expect(parking.amount).to eq(200)
+    end
+
+    it "61 mins should be 3 RMB" do
+      t = Time.now
+      parking = Parking.new(:parking_type => "guest",
+                            :start_at => t,
+                            :end_at => t + 61.minutes)
+      parking.calculate_amount
+      expect(parking.amount).to eq(300)
+    end
+
+    it "90 mins should be 3 RMB" do
+      t = Time.now
+      parking = Parking.new(:parking_type => "guest",
+                            :start_at => t,
+                            :end_at => t + 90.minutes)
+      parking.calculate_amount
+      expect(parking.amount).to eq(300)
+    end
+
+    it "120 mins should be 4 RMB" do
+      t = Time.now
+      parking = Parking.new(:parking_type => "guest",
+                            :start_at => t,
+                            :end_at => t + 120.minutes)
+      parking.calculate_amount
+      expect(parking.amount).to eq(400)
+    end
+  end
+end
